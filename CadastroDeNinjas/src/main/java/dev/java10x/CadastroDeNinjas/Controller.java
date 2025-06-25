3b92a6ae071bfbee4ddcd5a599fd799a225d2ab6
cauap <cauapablodev@gmail.com>
2025-06-25 09:57:22 -0300

    Criação do Controller

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "this is my first route";
+    }
+}

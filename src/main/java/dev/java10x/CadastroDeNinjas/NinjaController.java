ddcdfe925e17fdb34af1a8915cb335d96dc46997
cauapablodev <cauapablodev@gmail.com>
2025-07-25 14:29:43 -0300

    feat: rename NinjaControler, adding NinjaModel, adding dependency JPA

--- src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController// mapeia o arquivo e avisa que é um controller
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira msg na rota";

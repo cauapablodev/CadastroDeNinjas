808532ffc9c450e93a5e2dccb6e04fc274bdf8e4
cauapablodev <cauapablodev@gmail.com>
2025-07-29 17:05:51 -0300

    feat: adding all endPoint in controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController// mapeia o arquivo e avisa que é um controller
 @RequestMapping
@@ -11,4 +12,37 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira msg na rota";
     }
+
+    // Adicionar ninja (create)
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado com sucesso!";
+    }
+    // Mostrar todos os ninjas(read)
+
+    @GetMapping("/all")
+    public String mostrarTodosNinjas() {
+        return "Lista de todos os ninjas";
+    }
+    // Mostrar ninja por id (read)
+
+    @GetMapping("/todosID")
+    public String mostrarNinjaPorId() {
+        return "Mostrar ninja por ID";
+    }
+
+    //Alterar dados do ninja (update)
+
+    @PutMapping("/alterarID")
+    public String alterarNinja() {
+        return "Ninja alterado com sucesso!";
+    }
+    // Deletar ninja (delete)
+    @DeleteMapping("/deletarID")
+    public String deletarNinja() {
+        return "Ninja deletado com sucesso!";
+    }
+
 }
+

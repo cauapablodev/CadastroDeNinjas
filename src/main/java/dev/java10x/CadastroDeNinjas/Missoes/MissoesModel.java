3552ef8e9eaf6faaa6aa251726d69d8d45b74cb7
cauapablodev <cauapablodev@gmail.com>
2025-07-30 16:39:04 -0300

    fix: adding @NoArgsContructor, and @JsonIgnore for avoid serialization error

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,14 +1,17 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class MissoesModel {
@@ -25,5 +28,6 @@ public class MissoesModel {
 
     //@OneToMany uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }

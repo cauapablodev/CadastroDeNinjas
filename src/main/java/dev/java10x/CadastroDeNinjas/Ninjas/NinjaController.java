9daf7e600c5b0b836e3cc9e6aef3cbab36feb99e
cauapablodev <cauapablodev@gmail.com>
2025-07-30 16:42:48 -0300

    feat: changes the controller route to list the ninjas, and injects the service and repository dependency

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -5,9 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController// mapeia o arquivo e avisa que é um controller
 @RequestMapping("/ninjas") // mapeia a rota /ninjas
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira msg na rota";
@@ -21,13 +30,13 @@ public class NinjaController {
     }
     // Mostrar todos os ninjas(read)
 
-    @GetMapping("/ListaDeNinjas")
-    public String mostrarTodosNinjas() {
-        return "Lista de todos os ninjas";
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
     }
     // Mostrar ninja por id (read)
 
-    @GetMapping("/ListarID")
+    @GetMapping("/listarID")
     public String mostrarNinjaPorId() {
         return "Mostrar ninja por ID";
     }

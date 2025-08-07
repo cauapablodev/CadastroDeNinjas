f4828d62f1555bf7986efba1e525d4191cdcd684
cauapablodev <cauapablodev@gmail.com>
2025-08-07 18:01:27 -0300

    feat: MissoesController and MissoesService updated with DTO and MAPPER

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,26 +1,36 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
     private MissoesRepository missoesRepository; // Repositório para acessar os dados das missões
+    private MissoesMapper missoesMapper; // Mapeador para converter entre MissoesModel e MissoesDTO
 
     public MissoesService(MissoesRepository missoesRepository) { // Construtor que recebe o repositório de missões, inicializa o serviço de missões
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = new MissoesMapper(); // Inicializa o mapeador de missões
     }
 
     //Criar nova missão
-    public MissoesModel criarMissao(MissoesModel missao) { // Método para criar uma nova missão, recebe um objeto do tipo MissoesModel
-        return missoesRepository.save(missao); // Salva a missão no repositório e retorna o objeto salvo
+    public MissoesDTO criarMissao(MissoesDTO missaoDTO) { // Método para criar uma nova missão, recebe um objeto do tipo MissoesModel
+        MissoesModel missoes = missoesMapper.map(missaoDTO); // Mapeia o MissoesDTO para MissoesModel
+        missoesRepository.save(missoes);
+        return missoesMapper.map(missoes); // Mapeia o MissoesModel salvo de volta para MissoesDTO
     }
 
     //Listar todas as missões
 
-    public List<MissoesModel> listarMissoes() {
-        return missoesRepository.findAll();
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map) // Mapeia cada MissoesModel para MissoesDTO usando o mapeador
+                .collect(Collectors.toList()); // Coleta os resultados em uma lista
     }
 
     //Deletar missão
@@ -29,6 +39,17 @@ public class MissoesService {
     }
 
     //Alterar missão
+
+    public MissoesDTO alterarMissao(Long id, MissoesDTO missoesDTO) {
+        Optional<MissoesModel> missoesExistente = missoesRepository.findById(id);
+        if (missoesExistente.isPresent()) {
+            MissoesModel missoesAtualizado = missoesMapper.map(missoesDTO);
+            missoesAtualizado.setId(id);
+            MissoesModel missoesSalvo = missoesRepository.save(missoesAtualizado);
+            return missoesMapper.map(missoesSalvo);
+        }
+        return null;
+    }
 
 
 

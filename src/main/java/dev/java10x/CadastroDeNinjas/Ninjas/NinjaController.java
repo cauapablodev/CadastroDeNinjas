fbfb1bae86bfb16c0a79bb61868eae1ebc382d42
cauapablodev <cauapablodev@gmail.com>
2025-08-11 18:06:51 -0300

    refactor: adding responseEntity in NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,11 +1,15 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController// mapeia o arquivo e avisa que é um controller
 @RequestMapping("/ninjas") // mapeia a rota /ninjas
@@ -25,33 +29,57 @@ public class NinjaController {
     // Adicionar ninja (create)
 
     @PostMapping("/criar")
-    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) {
-        return ninjaService.criarNinja(ninja);
+    public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
+        NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Ninja criado com sucesso: " + novoNinja.getNome() + " com ID: " + novoNinja.getId());
+
     }
     // Mostrar todos os ninjas(read)
 
     @GetMapping("/listar")
-    public List<NinjaDTO> listarNinjas() {
-        return ninjaService.listarNinjas();
+    public ResponseEntity<List<NinjaDTO>> listarNinjas() {
+        List<NinjaDTO> listaDeNinjas = ninjaService.listarNinjas();
+        return ResponseEntity.ok(listaDeNinjas);
     }
 
     // Mostrar ninja por id (read)
     @GetMapping("/listarID/{id}") // {id} é um parâmetro de rota, que o usuário pode passar na URL
     // Exemplo: /listarID/1
-    public NinjaDTO listarNinjasPorId(@PathVariable Long id) {// @PathVariable indica que o parâmetro id é uma variável de rota
-        return ninjaService.listarNinjaPorId(id);
+    public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id) {// @PathVariable indica que o parâmetro id é uma variável de rota
+        NinjaDTO ninjaPorId = ninjaService.listarNinjaPorId(id);
+        if (ninjaPorId != null) {
+            return ResponseEntity.ok(ninjaPorId);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Ninja com ID " + id + " não encontrado.");
+        }
     }
 
     //Alterar dados do ninja (update)
 
     @PutMapping("/alterar/{id}") // {id} é um parâmetro de rota, que o usuário pode passar na URL
-    public NinjaDTO alterarNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
-        return ninjaService.atualizarNinja(id, ninjaAtualizado);
+    public ResponseEntity<?> alterarNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
+        NinjaDTO ninja = ninjaService.atualizarNinja(id, ninjaAtualizado);
+        if (ninja != null) {
+            return ResponseEntity.ok(ninja) ;
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Ninja com ID " + id + " não encontrado.");
+        }
     }
     // Deletar ninja (delete)
     @DeleteMapping("/deletar/{id}") // {id} é um parâmetro de rota, que o usuário pode passar na URL
-    public void deletarNinja(@PathVariable Long id) {
-        ninjaService.deletarNinja(id);
+    public ResponseEntity<String> deletarNinja(@PathVariable Long id) {
+        if (ninjaService.listarNinjaPorId(id) != null) {
+            ninjaService.deletarNinja(id);
+            return ResponseEntity.ok("Ninja deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Ninja com ID " + id + " não encontrado.");
+        }
+
+
     }
 
 }
